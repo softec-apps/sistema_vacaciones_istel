27c8b273075692452bb621262224c6d7dbe9197f
19MAX <maxisebas19@gmail.com>
2024-03-05 17:35:23 -0500

    Ort

--- admin/eliminar_users.php
+++ admin/eliminar_users.php
@@ -4,14 +4,22 @@ include_once "../conexion.php";
 
 
 if ($_POST) {
-    $id_usuario=$_POST["cliente_id"];
+    $id_usuario=(isset($_POST["cliente_id"])?$_POST["cliente_id"]:null);
+    if (empty($id_usuario)) {
+        create_flash_message(
+            'Problemas con el parámetro',
+            'error'
+        );
+
+        redirect(RUTA_ABSOLUTA . "admin/admin");
+    }
 
     try {
         $eliminar = eliminar_user($pdo,$id_usuario);
 
         if (strpos($eliminar, "Usuario Eliminado") !== false) {
             create_flash_message(
-                'Usuario Eliminado Correctamente ',
+                'Usuario eliminado correctamente ',
                 'success'
             );
 
@@ -19,7 +27,7 @@ if ($_POST) {
         }
     } catch (PDOException $e) {
         create_flash_message(
-            'Ocurrio un error con el sistema',
+            'Ocurrió un error con el sistema',
             'error'
         );
         redirect(RUTA_ABSOLUTA . "logout");
